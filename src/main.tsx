3668c0a19e781ae788afd2680083dc0fab7b76fa
Panth <panth.solanki@gmail.com>
2023-05-09 17:58:58 +0530

    material UI added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0496FF',
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
